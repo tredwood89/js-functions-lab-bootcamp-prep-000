76834a59a0748b6a20be43f22019b7f11a47c71f
tredwood89 <tredwood89@gmail.com>
2017-09-23 05:21:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ function happyHolidaysTo(name) {
 }
 
 
-function happyHolidayTo (holiday, name) {
+function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}`;
 }
 
-function holidayCountdown (days, holiday) {
+function holidayCountdown (holiday, days ) {
   return `It's ${days} days until ${holiday}!`;
 }
