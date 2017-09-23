c4667b82676286108e0045cccc5e22bb650cfacb
tredwood89 <tredwood89@gmail.com>
2017-09-23 05:24:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return `Happy holidays, ${name}`
+  return `Happy holidays, ${name}!`
 }
 
 
 function happyHolidayTo(holiday, name) {
-  return `Happy ${holiday}, ${name}`
+  return `Happy ${holiday}, ${name}!`
 }
 
 function holidayCountdown (holiday, days ) {
